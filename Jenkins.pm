7401fcf6db0b3c23e7f92c4739087dfa0920e34b
Coin Newell <colin.newell@gmail.com>
2015-04-24 18:23:34 +0100

    Revert "Change success criteria to check that last stable build = last build"
    This reverts commit 5a88ec23371f1e2b5f3dbfcbf8e87452fcf54486.
    
    This turned out to be a bad idea.  It meant it came up false when a
    build was running.

--- Jenkins.pm
+++ Jenkins.pm
@@ -22,26 +22,14 @@ sub is_alive
 sub success_check
 {
     my $self = shift;
-    # this only checks your views for jobs.
-    # it ignores any jobs not in views.
-    # that allowed me to ignore inconvenient failures
-    # without anything fancy.
     my $view_list = $self->{api}->current_status({ extra_params => { tree => 'views[name]' }});
     my @views = grep { $_ ne 'All' } map { $_->{name} } @{$view_list->{views}};
     my $ok = 1;
     for my $view (@views)
     {
-        my $view_jobs = $self->{api}->view_status($view, {
-                extra_params => {
-                    tree => 'jobs[name,color,lastBuild[number],lastStableBuild[number]]',
-                    depth => 1,
-                }
-            }
-        );
-        my $view_ok = all { $_ } map {
-            $_->{lastStableBuild} && $_->{lastBuild} &&
-            $_->{lastStableBuild}->{number} eq $_->{lastBuild}->{number}
-        } @{$view_jobs->{jobs}};
+        my $view_jobs = $self->{api}->view_status($view, { extra_params => { tree => 'jobs[name,color]' }});
+        # don't fail for aborted jobs.
+        my $view_ok = all { $_ =~ /^(blue|aborted)/ } map { $_->{color} } @{$view_jobs->{jobs}};
         $ok = $ok && $view_ok;
     }
     return $ok;
